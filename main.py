29eb3a4bafa00207dd615281691ceb0a83f1b00f
achille1017 <achillemarseille@gmail.com>
2023-04-26 23:36:35 +0200

    fix bug

--- main.py
+++ main.py
@@ -524,6 +524,7 @@ def get_qrcode(code, red):
     else:
         print(did)
         print(dossier["status"])
+        print(dossier)
         print(id_dossier)
         db.update_kyc(did, dossier["status"], id_dossier)
     try:
