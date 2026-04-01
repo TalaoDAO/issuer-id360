a0a4c0313e66d9754663626ae5a0fa1fb39d1fb3
Thierry Thevenet <thierry.thevenet@talao.io>
2026-04-01 17:37:11 +0200

    UPdate

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import redis
 import logging
 from flask_mobility import Mobility
 from routes import oidc
-#from routes import issuer_altme
+from routes import issuer_altme
 import os
 import message
 from flask_babel import Babel
@@ -31,7 +31,7 @@ mode = environment.currentMode(myenv)
 red = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 
-#issuer_altme.init_app(app, red, mode)
+issuer_altme.init_app(app, red, mode)
 #customer_api.init_app(app, red, mode)
 oidc.init_app(app, red, mode)
 
